d6a55b7b4db5b9d5a93170cc11eb0bcf4d68bf8b
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-06-08 15:32:12 +0530

    Snake movement and game over logic is added

--- script.js
+++ script.js
@@ -36,8 +36,60 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    function moveFood() {
+        let newX, newY;
+        do {
+            newX = Math.floor(Math.random() * ((arenaSize - cellSize)/cellSize)) * cellSize;
+            newY = Math.floor(Math.random() * ((arenaSize - cellSize)/cellSize)) * cellSize;
+        }while (
+            snake.some(snakeCell => snakeCell.x === newX && snakeCell.y === newY)
+        )
+        
+        food = {x: newX, y: newY}
+    }
+
+    function updateSnake() {
+        // calculate new co-ordinates the snake head will goto
+        const newHead = {x:snake[0].x + dx , y: snake[0].y + dy};
+        snake.unshift(newHead)  //add the new head
+        if(newHead.x === food.x && newHead.y === food.y){
+            // collision
+            score += 5;
+            moveFood();
+            // don't pop the tail
+        } else {
+            snake.pop();    // remove the last cell
+        }
+    }
+
+    function isGameOver() {
+        // check snake body hit
+
+        for( i = 1 ; i < snake.length ;i++) {
+            if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) return true; // game over
+        }
+
+        // check for wall hit conidition
+
+        const isHittingLeftWall = snake[0].x < 0;
+        const isHittingTopWall = snake[0].y < 0;
+        const isHittingRightWall = snake[0].x >= arenaSize;
+        const isHittingDownWall = snake[0].y >= arenaSize;
+
+        return isHittingDownWall || isHittingLeftWall || isHittingRightWall || isHittingTopWall //game over
+    }
+
     function gameLoop() {
         setInterval(() => {
+            if(!gameStarted) return;
+            // check for game over
+            if(isGameOver()) {
+                gameStarted = false;
+                alert(`Game Over, Score = ${score}`);
+                window.location.reload();
+                return ;
+            }
+            updateSnake()
             drawScoreBoard();
             drawFoodAndSnake();
         },1000)
@@ -50,6 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     function initiateGame() {
+
+
         const scoreBoard = document.createElement('div');
         scoreBoard.id = 'score-board';
         document.body.insertBefore(scoreBoard,gameArena);
